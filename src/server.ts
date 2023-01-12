10b1aab1b1a155768cad02fb6fcd2ff116490529
Valery Sl <110306381+ValerySl@users.noreply.github.com>
2023-01-12 17:11:39 +0300

    connection mongo

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import runApp from './helpers/runApp'
+import runMongo from './helpers/mongo'
 
 void (async () => {
+    console.log('Starting mongo')
+    await runMongo()
+    console.log('Mongo connected')
     await runApp()
     console.log('App running')
 })()
